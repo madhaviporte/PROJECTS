526123a4b4b32db5a0399989da5fe6d2e7455678
madhaviporte <madhaviporte2004@gamil.com>
2025-10-01 22:39:46 +0530

    my second commit

---
+++ Landing page/script.js
@@ -0,0 +1,30 @@
+// document.querySelectorAll(".imagecontainer")
+// .forEach(function(elem){
+//     gsap . to (elem,{
+//         ease: Expo .easeInOut,
+//         width: "100%",
+//         duration:2,
+//         stagger:2
+//     })
+// })
+
+gsap.to (".imagecontainer", {
+        ease: Expo .easeInOut,
+        width: "100%",
+        // duration:2,
+        stagger:2
+    } )
+
+    gsap.to (".text h1", {
+        ease: Expo.easeInOut,
+        // duration: 2,
+        stagger: 2,
+        top : 0
+    })
+    gsap.to (".text h1", {
+        delay: 2,
+        ease: Expo.easeInOut,
+        // duration: 2,
+        stagger: 2,
+        top : "-100%"
+    })
